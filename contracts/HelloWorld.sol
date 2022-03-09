15ea86ee46cab17dfb5288d710706c52d59897d2
"Taofeek " <"taofeek01@yahoo.com">
2022-03-10 00:16:29 +0100

    briiliant

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+
+uint256 public total;
+string public name;
+
+   constructor() {  
+   }
+
+function calculate( uint a, uint b) public {
+    total = a+b;
+}
+
+function setName(string memory _name) public {
+name = _name;
+}
+function retrieve() public view returns(string memory, uint) {
+    return (name, total);
+}
+
+}
